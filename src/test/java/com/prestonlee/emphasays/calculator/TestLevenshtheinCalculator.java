69c95f396a189842105f55cc6cdd9c2288c1be7a
Preston Lee <conmotto@gmail.com>
2013-12-01 19:38:11 -0700

    Minor changes for initial release.

--- src/test/java/com/prestonlee/emphasays/calculator/TestLevenshtheinCalculator.java
+++ src/test/java/com/prestonlee/emphasays/calculator/TestLevenshtheinCalculator.java
@@ -4,11 +4,19 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.prestonlee.emphasays.calculator.distance.LevenshteinDistanceCalculator;
+import com.prestonlee.emphasays.example.EmphasaysTutorial;
+
 public class TestLevenshtheinCalculator {
 
 	@Test
 	public void testDistance() {
-		fail("Not yet implemented"); // TODO
+		final LevenshteinDistanceCalculator calc = new LevenshteinDistanceCalculator();
+		int diff = (int)calc.calculate(EmphasaysTutorial.S1_SOURCE, EmphasaysTutorial.S1_TARGET);
+		assertSame(2, diff);
+		diff = (int)calc.calculate(EmphasaysTutorial.S2_SOURCE, EmphasaysTutorial.S2_TARGET);
+		assertSame(4, diff);
+		
 	}
 
 }
